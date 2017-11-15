85cf5feef8387d59a30a1b1cc3309e8663107275
Armand Delessert <armand.delessert@gmail.com>
2017-11-15 02:22:41 +0100

    Modif sur initializr.

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -3,14 +3,34 @@ User authentification
 */
 
 function login() {
-  var email = document.getElementById('login_email').value;
+  var username = document.getElementById('login_username').value;
   var password = document.getElementById('login_password').value;
-  var md5 = "e10adc3949ba59abbe56e057f20f883e";
-  var url = "https://demo6654639.mockable.io/api/login/" + email + "/" + md5;
+  var md5Pwd = "e10adc3949ba59abbe56e057f20f883e";
+  var url = "https://demo6654639.mockable.io/api/login/" + username + "/" + md5Pwd;
+  console.log("URL: " + url);
 
-  var xhttp = new XMLHttpRequest();
-  xmlHttp.open("POST", url, true);
-  xmlHttp.setRequestHeader("Content-type", "application/json");
-  xmlHttp.send();
-  var response = JSON.parse(xmlHttp.responseText);
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.onreadystatechange = function() {
+
+    // Débug
+    document.getElementById('debug_login').innerHTML =
+        "xhr.readyState: " + xhr.readyState + "</br>"
+        + "xhr.state: " + xhr.state + "</br>"
+        + "xhr: " + xhr;
+
+    //if (xhr.readyState === XMLHttpRequest.DONE && xhr.state === 200) {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.state === 200) {
+        var response = JSON.parse(xhr.responseText);
+        console.log("Token: " + response.token);
+        document.getElementById('debug_login').innerHTML = "Token: " + response.token;
+      }
+      else {
+        console.log("Response is not HTTP 200: " + response);
+      }
+    }
+  }
+  xhr.send();
 }
