278e20b89757b05e262565665b68618e9387e94f
Armand Delessert <armand.delessert@gmail.com>
2017-12-13 00:18:45 +0100

    Renommé les fichiers HTML et JavaScript.

--- nu/main.js
+++ nu/main.js
@@ -1,7 +1,113 @@
 /*
+Choix de l'API.
+*/
+// API définies
+const currentProdApi = 1;
+const currentDevApi = 2;
+const newApi = 3;
+
+// API utilisée
+const usedApi = newApi;
+
+// Constructions des URL
+if (usedApi == currentProdApi) {
+	const urlBase = "https://inventory.ing.he-arc.ch/api/rest/";
+
+	// URL pour le login
+	var loginUrl001 = "https://inventory.ing.he-arc.ch/api/login/";
+	var loginUrl002 = "/";
+	var loginUrl003 = "";
+
+	// URL pour l'affichage d'un produit
+	var urlGetProductDetails001 = urlBase;
+	var urlGetProductDetails002 = "/Product/info?productId=";
+	var urlGetProductDetails003 = "";
+
+	// URL pour la recherche d'un produit
+	var urlSearchProductByName001 = urlBase;
+	var urlSearchProductByName002 = "/search/";
+	var urlSearchProductByName003 = "/";
+
+	// URL pour l'emprunt d'un produit
+	var urlLoanProduct001 = urlBase;
+	var urlLoanProduct002 = "/Product/loan?productId=";
+	var urlLoanProduct003 = "&beginDate=";
+	var urlLoanProduct004 = "&endDate=";
+	var urlLoanProduct005 = "";
+
+	// URL pour le retour d'un produit
+	var urlReturnProduct001 = urlBase;
+	var urlReturnProduct002 = "/Product/returnProduct?productId=";
+	var urlReturnProduct003 = "";
+}
+else if (usedApi == currentDevApi) {
+	const urlBase = "http://inventory-dev.ing.he-arc.ch/api/rest/";
+
+	// URL pour le login
+	var loginUrl001 = "http://inventory-dev.ing.he-arc.ch/api/login/";
+	var loginUrl002 = "/";
+	var loginUrl003 = "";
+
+	// URL pour l'affichage d'un produit
+	var urlGetProductDetails001 = urlBase;
+	var urlGetProductDetails002 = "/Product/info?productId=";
+	var urlGetProductDetails003 = "";
+
+	// URL pour la recherche d'un produit
+	var urlSearchProductByName001 = urlBase;
+	var urlSearchProductByName002 = "/search/";
+	var urlSearchProductByName003 = "/";
+
+	// URL pour l'emprunt d'un produit
+	var urlLoanProduct001 = urlBase;
+	var urlLoanProduct002 = "/Product/loan?productId=";
+	var urlLoanProduct003 = "&beginDate=";
+	var urlLoanProduct004 = "&endDate=";
+	var urlLoanProduct005 = "";
+
+	// URL pour le retour d'un produit
+	var urlReturnProduct001 = urlBase;
+	var urlReturnProduct002 = "/Product/returnProduct?productId=";
+	var urlReturnProduct003 = "";
+}
+else if (usedApi == newApi) {
+	const urlBase = "https://demo6654639.mockable.io/api/rest/";
+
+	// URL pour le login
+	var loginUrl001 = "https://demo6654639.mockable.io/api/login/";
+	var loginUrl002 = "/";
+	var loginUrl003 = "/";
+
+	// URL pour l'affichage d'un produit
+	var urlGetProductDetails001 = urlBase;
+	var urlGetProductDetails002 = "/products/";
+	var urlGetProductDetails003 = "/details/";
+
+	// URL pour la recherche d'un produit
+	var urlSearchProductByName001 = urlBase;
+	var urlSearchProductByName002 = "/search/";
+	var urlSearchProductByName003 = "/";
+
+	// URL pour l'emprunt d'un produit
+	var urlLoanProduct001 = urlBase;
+	var urlLoanProduct002 = "/products/";
+	var urlLoanProduct003 = "/loan/";
+	var urlLoanProduct004 = "/";
+	var urlLoanProduct005 = "/";
+
+	// URL pour le retour d'un produit
+	var urlReturnProduct001 = urlBase;
+	var urlReturnProduct002 = "/products/";
+	var urlReturnProduct003 = "/return/";
+}
+
+
+/*
 Variables globales.
 */
 var theToken;
+var productId;
+var resultProductsId = [];
 
 
 /*
@@ -11,7 +117,6 @@ Fonction du onclick du bouton de login.
 function login_onclick() {
 	// Username: "devweb.user", "devweb.manager", "devweb.admin"
 	// Password: "123456"
-	// MD5 Password Hash: "e10adc3949ba59abbe56e057f20f883e"
 
 	var username = document.getElementById('login_username').value;
 	var password = document.getElementById('login_password').value;
@@ -24,21 +129,52 @@ Authentification de l'utilisateur.
 */
 function login(username, password) {
 	// Construction de l'URL de login
-	var loginUrlBase = "https://inventory-dev.ing.he-arc.ch/api/login/";
-	var loginUrl = loginUrlBase + username + "/" + password;
+	var loginUrl = makeUrlLogin(username, password);
 	console.log("Login with this username: " + username);
+	console.log("URL to login:\n" + loginUrl);
 
 	// Envoi de la requête de login
-	sendRequest(loginUrl, setToken);
+	sendRequest("GET", loginUrl, processLogin);
+}
+
+
+/*
+Construction de l'URL de login.
+*/
+function makeUrlLogin(username, password) {
+	return loginUrl001 + username + loginUrl002 + password + loginUrl003;
+}
+
+
+/*
+Actions au login de l'utilisateur.
+Callback de la fonction login().
+*/
+function processLogin(response) {
+	if (isRequestSuccess(response)) {
+		// Login réussi, enregistrement du token
+		setToken(response);
+
+		// Masquage du formulaire de login
+		document.getElementById('signIn').style.display = 'none';
+
+		// Affichage des autres commandes
+		document.getElementById('showProduct').style.display = 'block';
+		document.getElementById('scanWebCodeCamJS').style.display = 'block';
+		document.getElementById('searchProductByName').style.display = 'block';
+	}
+	else {
+		// Le login a échoué, affichage de l'erreur
+		printError('login_response', response);
+	}
 }
 
 
 /*
 Récupère le token de la réponse reçue.
-Callback de la fonction login().
 */
 function setToken(response) {
-	theToken = response.result;
+	theToken = response.token;
 	console.log("Received token: " + theToken);
 }
 
@@ -48,7 +184,7 @@ Récupération des détails d'un produit.
 Fonction du onclick du bouton.
 */
 function getProductDetails_onclick() {
-	var productId = document.getElementById('showProductDetails_productId').value;
+	productId = document.getElementById('showProductDetails_productId').value;
 	getProductDetails(productId);
 }
 
@@ -58,126 +194,50 @@ Récupération des détails d'un produit.
 */
 function getProductDetails(productId) {
 	// Construction de l'URL
-	var urlBase = "https://inventory-dev.ing.he-arc.ch/api/rest/";
-	var urlClass = "Product/";
-	var urlMethod = "info";
-
-	var urlParam = "?productId=";
-
-	var url = urlBase + theToken + "/" + urlClass + urlMethod + urlParam + productId;
+	var url = makeUrlGetProductDetails(productId);
 	console.log("URL to retrieve details of product with ID " + productId + ":\n" + url);
 
 	// Envoi de la requête
-	sendRequest(url, showProductName);
+	sendRequest("GET", url, processProductDetails);
 }
 
 
 /*
-Affichage du nom du produit.
+Construction de l'URL pour l'affichage d'un produit.
+*/
+function makeUrlGetProductDetails(productId) {
+	return urlGetProductDetails001 + theToken + urlGetProductDetails002 + productId + urlGetProductDetails003;
+}
+
+
+/*
+Affichage des détails du produit.
 Fonction de callback.
 */
-function showProductName(response) {
-	console.log("Product name: " + response.result.name);
-}
+function processProductDetails(response) {
+	if (isRequestSuccess(response)) {
+		console.log("Product ID: " + response.product.id
+				+ ", product name: " + response.product.name
+				+ ", product description: " + response.product.description
+				+ ", is loan: " + response.product.is_loan);
 
+		// Affichage des détails du produit
+		document.getElementById('getProductDetails_response').innerHTML =
+				"ID : " + response.product.id + "<br/>" +
+				"Nom : " + response.product.name + "<br/>" +
+				"Description : " + response.product.description + "<br/>" +
+				"Loué : " + response.product.is_loan;
 
-/*
-Emprunt d'un produit.
-Fonction du onclick du bouton.
-*/
-function loanProduct_onclick() {
-	var productId = document.getElementById('loanProduct_productId').value;
-	var beginDate = document.getElementById('beginDate_productId').value;
-	var endDate = document.getElementById('endDate_productId').value;
+		// Affichage de la location et du retour du produit
+		showLoanAndReturn();
+	}
+	else {
+		// Affichage de l'erreur
+		printError('getProductDetails_response', response);
 
-	beginDate = "2018-01-01";
-	endDate = "2019-02-01";
-	loanProduct(productId, beginDate, endDate);
-}
-
-
-/*
-Emprunt d'un produit.
-*/
-function loanProduct(productId, beginDate, endDate) {
-	// Construction de l'URL
-	var urlBase = "https://inventory-dev.ing.he-arc.ch/api/rest/";
-	var urlClass = "Product/";
-	var urlMethod = "loan";
-
-	var urlParam1 = "?productId=";
-	var urlParam2 = "&beginDate=";
-	var urlParam3 = "&endDate=";
-
-	var url = urlBase + theToken + "/" + urlClass + urlMethod
-			+ urlParam1 + productId
-			+ urlParam2 + beginDate
-			+ urlParam3 + endDate;
-	console.log("URL to loan the product with ID " + productId
-			+ " from the " + beginDate
-			+ " to the " + endDate
-			+ ":\n" + url);
-
-	// Envoi de la requête
-	sendRequest(url, showResponse);
-}
-
-
-/*
-Retour d'un produit.
-Fonction du onclick du bouton.
-*/
-function returnProduct_onclick(productId) {
-	var productId = document.getElementById('returnProduct_productId').value;
-	returnProduct(productId);
-}
-
-
-/*
-Retour d'un produit.
-*/
-function returnProduct(productId) {
-	// Construction de l'URL
-	var urlBase = "https://inventory-dev.ing.he-arc.ch/api/rest/";
-	var urlClass = "Product/";
-	var urlMethod = "returnProduct";
-
-	var urlParam = "?productId=";
-
-	var url = urlBase + theToken + "/" + urlClass + urlMethod + urlParam + productId;
-	console.log("URL to return the product with ID " + productId + ":\n" + url);
-
-	// Envoi de la requête
-	sendRequest(url, showResponse);
-}
-
-
-/*
-Recherche d'un produit par son ID.
-Fonction du onclick du bouton.
-*/
-function searchProductById_onclick() {
-	var productId = document.getElementById('searchProductById_productId').value;
-	searchProductById(productId);
-}
-
-
-/*
-Recherche d'un produit par son ID.
-*/
-function searchProductById(productId) {
-	// Construction de l'URL
-	var urlBase = "https://inventory-dev.ing.he-arc.ch/api/rest/";
-	var urlClass = "Product/";
-	var urlMethod = "searchById";
-
-	var urlParam = "?productId=";
-
-	var url = urlBase + theToken + "/" + urlClass + urlMethod + urlParam + productId;
-	console.log("URL to search the product with ID " + productId + ":\n" + url);
-
-	// Envoi de la requête
-	sendRequest(url, showResponse);
+		// Masquage de la location et du retour du produit
+		hideLoanAndReturn();
+	}
 }
 
 
@@ -196,35 +256,178 @@ Recherche d'un produit par son nom.
 */
 function searchProductByName(term) {
 	// Construction de l'URL
-	var urlBase = "https://inventory-dev.ing.he-arc.ch/api/rest/";
-	var urlClass = "Product/";
-	var urlMethod = "search";
-
-	var urlParam = "?term=";
-
-	var url = urlBase + theToken + "/" + urlClass + urlMethod + urlParam + term;
+	var url = makeUrlSearchProductByName(term);
 	console.log("URL to search the product with the name \"" + term + "\":\n" + url);
 
 	// Envoi de la requête
-	sendRequest(url, showResponse);
+	sendRequest("GET", url, processSearchProductByName);
 }
 
 
 /*
-Affichage de la réponse.
+Construction de l'URL pour la recherche d'un produit.
+*/
+function makeUrlSearchProductByName(term) {
+	return urlSearchProductByName001 + theToken + urlSearchProductByName002 + term + urlSearchProductByName003;
+}
+
+
+/*
+Affichage des produits trouvés.
 Fonction de callback.
 */
-function showResponse(response) {
-	console.log("Last response:\n" + JSON.stringify(response));
+function processSearchProductByName(response) {
+	if (isRequestSuccess(response)) {
+		console.log("Number of results: " + response.number_results);
+		console.log("Complete response:\n" + JSON.stringify(response));
+
+		// Affichage des résultats
+		var responseSection = document.getElementById('searchProductByName_response');
+		responseSection.innerHTML = "Nombre de résultats : " + response.number_results;
+		responseSection.innerHTML += "</br>";
+
+		responseSection.innerHTML += "<ul id=\"searchProductByName_list\"></ul>";
+		var resultsListSection = document.getElementById('searchProductByName_list');
+		for (i = 0; i < response.number_results; ++i) {
+			var resultId = "searchProductByName_result" + i;
+
+			resultsListSection.innerHTML +=
+					"<li>" + "<a " + "id=\"" + resultId + "\"" + " href=\"" + "#" + "\">"
+					+ response.results[i].id
+					+ ": "
+					+ response.results[i].name
+					+ "</a>" + "</li>";
+		}
+
+		for (i = 0; i < response.number_results; ++i) {
+			resultProductsId[i] = response.results[i].id;
+
+			var resultId = "searchProductByName_result" + i;
+			document.getElementById(resultId).onclick = function(mouseEvent) {
+				// Recherche du produit sélectionné
+				productId = resultProductsId[mouseEvent.srcElement.id["searchProductByName_result".length]];
+				getProductDetails(productId);
+			}
+		}
+	}
+	else {
+		// Affichage de l'erreur
+		printError('searchProductByName_response', response);
+	}
+}
+
+
+/*
+Emprunt d'un produit.
+Fonction du onclick du bouton.
+*/
+function loanProduct_onclick() {
+	var beginDate = document.getElementById('beginDate_productId').value;
+	var endDate = document.getElementById('endDate_productId').value;
+
+	loanProduct(productId, beginDate, endDate);
+}
+
+
+/*
+Emprunt d'un produit.
+*/
+function loanProduct(productId, beginDate, endDate) {
+	// Construction de l'URL
+	var url = makeUrlLoanProduct(productId, beginDate, endDate);
+	console.log("URL to loan the product with ID " + productId
+			+ " from the " + beginDate
+			+ " to the " + endDate
+			+ ":\n" + url);
+
+	// Envoi de la requête
+	sendRequest("PUT", url, processLoanProduct);
+}
+
+
+/*
+Construction de l'URL pour l'emprunt d'un produit.
+*/
+function makeUrlLoanProduct(productId, beginDate, endDate) {
+	return urlLoanProduct001 + theToken + urlLoanProduct002 + productId
+			+ urlLoanProduct003 + beginDate + urlLoanProduct004 + endDate + urlLoanProduct005;
+}
+
+
+/*
+Affichage des produits trouvés.
+Fonction de callback.
+*/
+function processLoanProduct(response) {
+	if (isRequestSuccess(response)) {
+		console.log("Loan success!");
+
+		// Affichage des résultats
+		document.getElementById('loanProduct_response').innerHTML =
+				"Emprunt du produit réussi !";
+	}
+	else {
+		// Affichage de l'erreur
+		printError('loanProduct_response', response);
+	}
+}
+
+
+/*
+Retour d'un produit.
+Fonction du onclick du bouton.
+*/
+function returnProduct_onclick() {
+	returnProduct(productId);
+}
+
+
+/*
+Retour d'un produit.
+*/
+function returnProduct(productId) {
+	// Construction de l'URL
+	var url = makeUrlReturnProduct(productId);
+	console.log("URL to return the product with ID " + productId + ":\n" + url);
+
+	// Envoi de la requête
+	sendRequest("PUT", url, processReturnProduct);
+}
+
+
+/*
+Construction de l'URL pour le retour d'un produit.
+*/
+function makeUrlReturnProduct(productId) {
+	return urlReturnProduct001 + theToken + urlReturnProduct002 + productId + urlReturnProduct003;
+}
+
+
+/*
+Affichage des produits trouvés.
+Fonction de callback.
+*/
+function processReturnProduct(response) {
+	if (isRequestSuccess(response)) {
+		console.log("Return success!");
+
+		// Affichage des résultats
+		document.getElementById('returnProduct_response').innerHTML =
+				"Retour du produit réussi !";
+	}
+	else {
+		// Affichage de l'erreur
+		printError('returnProduct_response', response);
+	}
 }
 
 
 /*
 Envoi d'une requête.
 */
-function sendRequest(url, callback) {
+function sendRequest(method, url, callback) {
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url, true);
+	xhr.open(method, url, true);
 	xhr.setRequestHeader("Content-type", "application/json");
 	xhr.onreadystatechange = function() {
 		sendRequest_callback(xhr, callback);
@@ -246,4 +449,65 @@ function sendRequest_callback(xhr, callback) {
 			callback(response);
 		}
 	}
+}
+
+
+/*
+Vérification de la réponse.
+*/
+function isRequestSuccess(response) {
+	return response.success;
+}
+
+
+/*
+Affichage des sections Loan et Return.
+*/
+function showLoanAndReturn() {
+	document.getElementById('loanProduct').style.display = 'block';
+	document.getElementById('returnProduct').style.display = 'block';
+}
+
+
+/*
+Masquage des sections Loan et Return.
+*/
+function hideLoanAndReturn() {
+	document.getElementById('loanProduct').style.display = 'none';
+	document.getElementById('returnProduct').style.display = 'none';
+}
+
+
+/*
+Affichage de l'erreur.
+*/
+function printError(elementId, response) {
+	document.getElementById(elementId).innerHTML = response.error;
+	console.log("Error: " + response.error);
+}
+
+
+/*
+Scanner WebCodeCanJS
+Démarrage du scan de codes-barres et codes QR.
+*/
+function scanWebCodeCamJS() {
+	var txt = "innerText" in HTMLElement.prototype ? "innerText" : "textContent";
+	var arg = {
+		resultFunction: function(result) {
+			// Affichage du contenu du code QR
+			document.getElementById("scanWebCodeCamJS_response").innerHTML = "Found " + result.format + ": " + result.code;
+
+			// Recherche du produit correspondant
+			productId = result.code;
+			getProductDetails(productId);
+		}
+	};
+	var decoder = new WebCodeCamJS("canvas").buildSelectMenu('select', 'environment|back').init(arg).play();
+	/*  Without visible select menu
+			var decoder = new WebCodeCamJS("canvas").buildSelectMenu(document.createElement('select'), 'environment|back').init(arg).play();
+	*/
+	document.querySelector('select').addEventListener('change', function() {
+		decoder.stop().play();
+	});
 }
